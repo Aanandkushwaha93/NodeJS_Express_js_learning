48b990bc0edf2081bbb88d34418c549404c6ed96
Aanand Kushwaha <aananadkushwaha@393gmail.com>
2026-07-28 00:32:36 +0530

    checking age or ip middleware

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-import express from 'express'
+import express from 'express';
 const app = express();
-function checking(req,resp,next){
-    console.log(req.url)
-    next()
+// function check(req, resp, next) {
+//     if (!req.query.age || req.query.age < 18) {
+//         resp.send("<h1>You are not eligible</h1>")
+//     } else {
+//         next();
+//     }
+// }
+function ipCheck(req,resp,next) {
+    const ip  = req.socket.remoteAddress;
+    console.log(ip)
+    if(ip.includes('10.238.212.19')){
+        resp.send('You can not access this page')
+    }else{
+        next()
+    }
 }
-app.use(checking)
+// app.use(check)
+app.use(ipCheck)
 app.get('/', (req, resp) => {
-resp.send('home')
+    resp.send("<h1>This is home page</h1>")
 })
-app.get('/about', (req, resp) => {
-resp.send('about')
+app.get('/user', (req, resp) => {
+    resp.send("<h1>This is user page</h1>")
 })
-app.listen(3300)
+app.get('/admin', (req, resp) => {
+    resp.send("<h1>This is user page</h1>")
+})
+app.listen(2200)
