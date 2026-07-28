ee9230a2ca9cc2de1ee019e780ec5803978139f8
Aanand Kushwaha <aananadkushwaha@393gmail.com>
2026-07-28 23:10:51 +0530

    built in middleware in nodejs

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
 import express from 'express';
+import path from 'path';
+
 const app = express();
-
-function checkAgeRouteMiddleware(req,resp,next) {
-    if(!req.query.age||req.query.age<18){
-        console.log(req.query.age)
-        resp.send('You are not allowed to this site')
-    }else{
-        next()
-    }
-}
-function checkUrlRouteMiddleware(req,resp,next) {
-    console.log('This is Url checker',req.url)
-    next();
-}
-
+app.use(express.urlencoded({extended:false}))
+app.use(express.static('public/'))
 app.get('/', (req, resp) => {
-    resp.send('<h1>Home page</h1>')
+    const filePath = path.resolve('view/home.html')
+    resp.sendFile(filePath)
 })
 app.get('/login', (req, resp) => {
-    resp.send('<h1>Login page</h1>')
+    resp.send(`
+         <form action="/submit" method="post">
+        <input type="text" name="name"><br/>
+        <input type="text" name="password" type="password"><br/>
+        <button type="submit">Submit </button>
+    </form>`)
 })
-app.get('/user',checkAgeRouteMiddleware,checkUrlRouteMiddleware, (req, resp) => {
-    resp.send('<h1>User page</h1>')
+app.post('/submit', (req, resp) => {
+    console.log('data submit',req.body)
+    resp.send('<h1>Data submit page </h1>')
 })
-app.get('/products',checkAgeRouteMiddleware,checkUrlRouteMiddleware, (req, resp) => {
-    resp.send('<h1>Product page</h1>')
-})
-app.listen(2500);
+app.listen(4500)
