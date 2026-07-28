079478b9cc8ab2324a3d8e204f7e4b88a685667a
Aanand Kushwaha <aananadkushwaha@393gmail.com>
2026-07-28 23:54:32 +0530

    morgan for http logs | error handle middleware

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 import express from 'express';
-import path from 'path';
-
+import morgan from 'morgan';
 const app = express();
-app.use(express.urlencoded({extended:false}))
-app.use(express.static('public/'))
-app.get('/', (req, resp) => {
-    const filePath = path.resolve('view/home.html')
-    resp.sendFile(filePath)
+// app.use(morgan('dev'))
+app.get('', (req, resp) => {
+    resp.send('<h1>Home </h1>')
 })
 app.get('/login', (req, resp) => {
-    resp.send(`
-         <form action="/submit" method="post">
-        <input type="text" name="name"><br/>
-        <input type="text" name="password" type="password"><br/>
-        <button type="submit">Submit </button>
-    </form>`)
+    resp.send('<h1>Login Page </h1>')
 })
-app.post('/submit', (req, resp) => {
-    console.log('data submit',req.body)
-    resp.send('<h1>Data submit page </h1>')
+app.get('/wait', (req, resp) => {
+    setTimeout(() => {
+        resp.send('<h1>Wait page </h1>')
+    }, 2000);
 })
-app.listen(4500)
+// Error handling page 
+app.get('/error', (req, resp) => {
+    const error = new Error('');
+    error.status = 404;
+    next(error);
+    resp.send('<h1>This is error page</h1>')
+})
+app.get('/user', (req, resp, next) => {
+    resp.sends('THis is new error')
+})
+
+app.use((error, req, resp, next) => {
+    // chahe to file bhi send kar sakte he
+    resp.status(error.status || 500).send('Try after some time')
+})
+app.listen(2100)
