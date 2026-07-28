3a85fb2da98bf2c8a4c044e34521896d5806f86d
Aanand Kushwaha <aananadkushwaha@393gmail.com>
2026-07-29 01:06:00 +0530

    Handle form data in Template engine ejs file

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from "express";
 const app = express();
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
+// app.get('', (req, resp) => {
+//     resp.render('home',{name:'Anil sidhu',email:'aanandkushwah@gmail.com'})
+// })
 
-app.get('', (req, resp) => {
-    resp.render('home',{name:'Anil sidhu',email:'aanandkushwah@gmail.com'})
+// Add form data in Template engine
+app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'ejs')
+app.get('/login', (req, resp) => {
+    resp.render('addUser')
+})
+app.post('/submit-user', (req, resp) => {
+    resp.render('SubmitUser',req.body)
 })
 app.listen(4500);
