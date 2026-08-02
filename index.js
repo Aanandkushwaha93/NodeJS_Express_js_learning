6f91e167bf39e539cf62b082404fefcb743e5150
Aanand Kushwaha <aananadkushwaha@393gmail.com>
2026-08-03 01:55:13 +0530

    Display mongodb data on UI

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const dbName = "school";
 const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url);
 const app = express();
-
-async function dbConnection() {
+app.set('view engine','ejs')
+app.get('/', async (req, resp) => {
     await client.connect();
     const db = client.db(dbName);
     const collection = db.collection('student');
-    const result = await collection.find().toArray()
-    console.log(result)
-}
-dbConnection();
+    const students = await collection.find().toArray()
+    console.log(students)
+    resp.render('students',{students})
+})
+
 app.listen(3200);
