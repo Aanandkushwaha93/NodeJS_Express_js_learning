1e6411419ea30705f3ba9d885c4d71bcc86a7ca4
Aanand Kushwaha <aananadkushwaha@393gmail.com>
2026-08-05 04:39:13 +0530

    add, delete,

--- index.js
+++ index.js
@@ -4,14 +4,31 @@ const dbName = "school";
 const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url);
 const app = express();
-app.set('view engine','ejs')
-app.get('/', async (req, resp) => {
-    await client.connect();
-    const db = client.db(dbName);
-    const collection = db.collection('student');
-    const students = await collection.find().toArray()
-    console.log(students)
-    resp.render('students',{students})
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+client.connect().then((connection) => {
+    const db = connection.db(dbName);
+    app.get('/api', async (req, resp) => {
+        const collection = db.collection('student');
+        const students = await collection.find().toArray();
+        resp.send(students);
+    })
+    app.get('/ui', async (req, resp) => {
+        const collection = db.collection('student');
+        const students = await collection.find().toArray();
+        resp.render('students', { students });
+    })
+    app.get('/add', (req, resp) => {
+        resp.render('add-student')
+    });
+    app.post('/add-user', async (req, resp) => {
+        // console.log(req.body);
+        const collection = db.collection('student');
+        const result = await collection.insertOne(req.body);
+        console.log(result);
+        resp.send('data saved');
+    })
 })
 
 app.listen(3200);
