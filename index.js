10f21e208a84163b2992bef1ce90be1da9bc1ad5
Aanand Kushwaha <aananadkushwaha@393gmail.com>
2026-08-08 22:46:22 +0530

    upload file using multer

--- index.js
+++ index.js
@@ -1,47 +1,29 @@
-import mongoose from "mongoose";
-import express from "express";
-import studentModel from "./model/studentModel.js";
-import cors from 'cors';
+import express from 'express';
+import multer from 'multer';
+
 const app = express();
-await mongoose.connect('mongodb://localhost:27017/school').then(() => {
-    console.log('___connect___')
-})
-app.use(cors())
-app.use(express.json())
-app.get('/', async (req, resp) => {
-    const result = await studentModel.find()
-    resp.send(result);
-})
-
-app.post('/save', async (req, resp) => {
-    const { name, age, email } = req.body;
-    if (!name || !email || !age) {
-        resp.send({ message: "Data not stored, please provide all field", success: false, storedInfo: null })
+const storage = multer.diskStorage({
+    destination:function (req,file,cb){
+        cb(null,'upload')
+    },
+    filename:function (req,file,cb){
+        cb(null,file.originalname)
     }
-    const studentData = await studentModel.create(req.body)
-    resp.send({ message: 'data stored', success: true, storedInfo: studentData });
-    console.log(studentData)
-});
-app.put('/update/:id', async (req, resp) => {
-    const id = req.params.id;
-    console.log(req.body, id);
-    const studentData = await studentModel.findByIdAndUpdate(id, {
-        ...req.body
-    })
-    resp.send({
-        message: 'Data Updated Succesfully',
-        success: true,
-        info: studentData
-    })
-
 })
-app.delete('/delete/:id', async(req, resp) => {
-    const id = req.params.id;
-    const studentData = await studentModel.findByIdAndDelete(id);
+const upload = multer({storage});
+app.get('/', (req, resp) => {
+    resp.send(`
+        <form action='/upload' method='post' enctype='multipart/form-data'>
+        <input type='file' name='myfile' />
+        <button> Upload File</button>
+        </form>
+        `)
+})
+app.post('/upload', upload.single('myfile'),  (req, resp)=> {
     resp.send({
-        message: 'Data Deleted',
-        success: true,
-        info: studentData
+        message:'File uploaded',
+        success:true,
+        info:req.file
     })
 })
-app.listen(2300)
+app.listen(2200)
