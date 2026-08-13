d9bc0a27cfe79df20c0c1551faaffbbf41ca749a
Aanand Kushwaha <aananadkushwaha@393gmail.com>
2026-08-14 00:07:17 +0530

    set and get cookie

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-import express from "express";
-import { MongoClient } from "mongodb";
-const url = 'mongodb+srv://aanandkushwaha:c0x7GONPVhTKh9to@cluster0.alfcauh.mongodb.net/?appName=Cluster0';
-const database = 'school';
-const collection = 'student';
-const client = new MongoClient(url);
-client.connect().then(()=>[
-    console.log('.......connect.......')
-])
-async function dbConnection() {
-    const db = client.db(database);
-    const collectionResult = db.collection(collection);
-    const result = await collectionResult.find().toArray();
-    console.log(result);    
-}
-dbConnection();
+import express from 'express';
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended:true}));
+
+app.get('/login', (req, resp) => {
+    resp.render('login')
+})
+app.post('/profile', (req, resp) => {
+    resp.setHeader('Set-Cookie', 'login=true')
+    resp.setHeader("Set-Cookie", "name=" + req.body.name)
+    resp.render('profile')
+})
+app.get('/', (req, resp) => { 
+    let cookieResult = req.get('cookie');
+    cookieResult = cookieResult.split(';');
+    cookieResult = cookieResult[1].split('=');
+    resp.render('home',{name:cookieResult[1]});
+}) 
+app.listen(2200);
